fd5615ac6a00add921aa56b63f165bed6a4ce63c
Manuel Hässig <manuel@haessig.org>
2022-12-02 17:53:50 +0100

    Solved 2022/01

--- 2022/01/src/main.rs
+++ 2022/01/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    error::Error,
+    fs::File,
+    io::{BufRead, BufReader}
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let f = File::open("input.txt")?;
+    let r = BufReader::new(f);
+
+    let mut calories = Vec::<u64>::new();
+
+    let mut sum: u64 = 0;
+    for line in r.lines() {
+        let line = line?;
+        if line.is_empty() {
+            calories.push(sum);
+            sum = 0;
+        } else {
+            sum += line.parse::<u64>()?;
+        }
+    }
+
+    calories.sort();
+    calories.reverse();
+    println!("Max calories: {}", calories[0]);
+    println!("Max backup calories: {}", calories.iter().take(3).sum::<u64>());
+
+    Ok(())
 }
