75dac8bfd7b7d05f92bb193d5cee523c267efecf
Manuel Hässig <manuel@haessig.org>
2024-12-10 23:13:39 +0100

    AoC 2024, day 10, part 2

--- 2024/10/src/main.rs
+++ 2024/10/src/main.rs
@@ -3,6 +3,7 @@ use std::{
     error::Error,
     fs::File,
     io::{BufRead, BufReader},
+    vec,
 };
 
 type TopoMap = Vec<Vec<u8>>;
@@ -37,6 +38,36 @@ fn walk_trail(coord: Coord, seen: &mut HashSet<Coord>, map: &TopoMap) {
     }
 }
 
+fn find_trails(coord: Coord, map: &TopoMap) -> Vec<Vec<Coord>> {
+    let (x, y) = coord;
+    let altitude = map[y][x];
+
+    if altitude == 9 {
+        return vec![vec![coord]];
+    }
+
+    let mut paths = Vec::new();
+    for xn in x.saturating_sub(1)..=usize::min(x + 1, map[0].len() - 1) {
+        for yn in y.saturating_sub(1)..=usize::min(y + 1, map.len() - 1) {
+            if xn != x && yn != y {
+                continue;
+            }
+
+            let c = (xn, yn);
+
+            if map[yn][xn] == altitude + 1 {
+                let mut found = find_trails(c, map);
+                for p in found.iter_mut() {
+                    p.push(coord);
+                    paths.push(p.clone())
+                }
+            }
+        }
+    }
+
+    paths
+}
+
 fn solution(r: BufReader<File>) -> Result<(u64, u64), Box<dyn Error>> {
     let mut grid = Vec::<Vec<u8>>::new();
     let mut trailheads = Vec::<Coord>::new();
@@ -68,7 +99,10 @@ fn solution(r: BufReader<File>) -> Result<(u64, u64), Box<dyn Error>> {
             seen.intersection(&tops).count() as u64
         })
         .sum();
-    let part2 = 0;
+    let part2 = trailheads
+        .iter()
+        .map(|th| find_trails(*th, &grid).len() as u64)
+        .sum();
 
     Ok((part1, part2))
 }
@@ -86,17 +120,9 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 #[test]
-fn smol_test() {
-    let f = File::open("small_test_input.txt").unwrap();
-    let r = BufReader::new(f);
-
-    assert_eq!(solution(r).unwrap(), (1, 0))
-}
-
-#[test]
 fn test() {
     let f = File::open("test_input.txt").unwrap();
     let r = BufReader::new(f);
 
-    assert_eq!(solution(r).unwrap(), (36, 0))
+    assert_eq!(solution(r).unwrap(), (36, 81))
 }
