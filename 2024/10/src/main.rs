b5033474f3cf679d4f64505ef7691ba64a58ea9f
Manuel Hässig <manuel@haessig.org>
2024-12-10 22:59:40 +0100

    AoC 2024, day 10, part 1

---
+++ 2024/10/src/main.rs
@@ -0,0 +1,102 @@
+use std::{
+    collections::HashSet,
+    error::Error,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+type TopoMap = Vec<Vec<u8>>;
+type Coord = (usize, usize);
+
+/// Recursive DFS starting from a trailhead.
+fn walk_trail(coord: Coord, seen: &mut HashSet<Coord>, map: &TopoMap) {
+    seen.insert(coord);
+    let (x, y) = coord;
+    let altitude = map[y][x];
+
+    if altitude == 9 {
+        return;
+    }
+
+    for xn in x.saturating_sub(1)..=usize::min(x + 1, map[0].len() - 1) {
+        for yn in y.saturating_sub(1)..=usize::min(y + 1, map.len() - 1) {
+            // No diagonals!
+            if xn != x && yn != y {
+                continue;
+            }
+
+            let c = (xn, yn);
+            if seen.contains(&c) {
+                continue;
+            }
+
+            if map[yn][xn] == altitude + 1 {
+                walk_trail(c, seen, map);
+            }
+        }
+    }
+}
+
+fn solution(r: BufReader<File>) -> Result<(u64, u64), Box<dyn Error>> {
+    let mut grid = Vec::<Vec<u8>>::new();
+    let mut trailheads = Vec::<Coord>::new();
+    let mut tops = HashSet::<Coord>::new();
+
+    for (y, line) in r.lines().enumerate() {
+        let line = line?;
+
+        let mut grid_line = Vec::new();
+        for (x, c) in line.char_indices() {
+            let n = c.to_string().parse()?;
+            grid_line.push(n);
+
+            if n == 0 {
+                trailheads.push((x, y));
+            } else if n == 9 {
+                tops.insert((x, y));
+            }
+        }
+
+        grid.push(grid_line);
+    }
+
+    let part1 = trailheads
+        .iter()
+        .map(|th| {
+            let mut seen = HashSet::new();
+            walk_trail(*th, &mut seen, &grid);
+            seen.intersection(&tops).count() as u64
+        })
+        .sum();
+    let part2 = 0;
+
+    Ok((part1, part2))
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let f = File::open("input.txt")?;
+    let r = BufReader::new(f);
+
+    let (part1, part2) = solution(r)?;
+
+    println!("Part 1: {part1}");
+    println!("Part 2: {part2}");
+
+    Ok(())
+}
+
+#[test]
+fn smol_test() {
+    let f = File::open("small_test_input.txt").unwrap();
+    let r = BufReader::new(f);
+
+    assert_eq!(solution(r).unwrap(), (1, 0))
+}
+
+#[test]
+fn test() {
+    let f = File::open("test_input.txt").unwrap();
+    let r = BufReader::new(f);
+
+    assert_eq!(solution(r).unwrap(), (36, 0))
+}
